8f278ae371e344119246559b6db4da04afb7b635
NdivhoNemz <ndivhonematandani@gmail.com>
2023-02-16 04:58:47 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio>
+/**
+ * main - A C program that prints using printf
+ * Return: 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
