82bc823c55a97a4348e22a4a0edb1e7dcb8ed209
NdivhoNemz <ndivhonematandani@gmail.com>
2023-02-17 12:42:08 -0800

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a < '9'; a++)
+	{
+	for (b = a + 1; n <= '9'; b++)
+	{
+	for (c = b + 1; c <= '9'; c++)
+	{
+	if ((b != a) != c)
+	putchar(a);
+	putchar(b);
+	putchar(c);
+	}
+	if (d == '7' && p == '8')
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	putchar('\n');
+	return (0);
+}
