73b10b3cb244f061337f8cef5e69ad9a3ff394a2
NdivhoNemz <ndivhonematandani@gmail.com>
2023-09-07 10:48:55 -0700

    Update compiles

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,11 @@
  */
 
 int main(void)
+
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
+/* \" is used to escape the needed " */
+/* puts always prints in a new line */
