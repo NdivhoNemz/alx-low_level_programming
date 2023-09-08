01fcb23068207e21716c1aca08f244bdef2e5b37
NdivhoNemz <ndivhonematandani@gmail.com>
2023-09-08 04:21:12 -0700

    find soultion with write

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - A C program that prints exactly
+ * Return: 1
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+
+	/* used write as functions listed in printf or puts are prohibited */
+
+	return (1);
+}
