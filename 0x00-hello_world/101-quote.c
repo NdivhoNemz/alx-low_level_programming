d3e43526f9e8644c254e44eb3fe9b7a23213fbbd
NdivhoNemz <ndivhonematandani@gmail.com>
2023-09-11 07:56:40 -0700

    write function and stderr, 2 for stderr and 59 for string

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,12 @@
 
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	/* used write as functions listed in printf or puts are prohibited */
+	/* 2 is a file descritpor used for standard error */
+	/* 59 the length of the string + 1, (+ 1 = special char '\0')*/
+	/* return ?????? */
+	/* return value above 0 is an error */
 
 	return (1);
 }
