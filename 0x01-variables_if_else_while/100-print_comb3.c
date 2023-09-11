4e4e4a73a4ccb904caaf5bba9096dadeab4e4c6d
NdivhoNemz <ndivhonematandani@gmail.com>
2023-09-11 11:10:53 -0700

    Write a program that prints all possible different combinations of two digits.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,18 +9,37 @@ int main(void)
 {
 	int digit_1, digit_2;
 
+	/**
+	 * "digit_1 = '0'" begings print from number '0'
+	 * "digit_1 < '9'" will end print at number '8'
+	 * "digit_1++" will increase '0' one digit at a time until '9'
+	 */
 	for (digit_1 = '0'; digit_1 < '9'; digit_1++)
-	for (digit_2 = digit_1 + 1; digit_2 <= '9'; digit_2++)
-
-		if (digit_2 != digit_1)
-			putchar(digit_1);
-	putchar(digit_2);
-
-	if (digit_1 == '8' && digit_2 == '9')
-		putchar(',');
-	putchar(' ');
-
-	putchar('\n');
-
+	{
+		/**
+	 * "digit_2 = digit_1 + 1" begings print from number '1'
+	 * "digit_2 <= '9'" will end print at number '9'
+	 * "digit_2++" will increase '0' one digit at a time until '9'
+	 */
+		for (digit_2 = digit_1 + 1; digit_2 <= '9'; digit_2++)
+		{
+			/* "digit_2 != digit_1" will avoid dupicate numbers */
+			if (digit_2 != digit_1)
+			{
+				putchar(digit_1);
+				putchar(digit_2);
+			}
+			/* "digit_1 == '8' && digit_2 == '9'" to exclude sep*/
+			if (digit_1 == '8' && digit_2 == '9')
+			{
+				putchar('\n');
+			}
+			else
+			{	/* seperate with space and , */
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 	return (0);
 }
