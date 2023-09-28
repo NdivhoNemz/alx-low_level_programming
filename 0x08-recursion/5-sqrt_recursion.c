074cbbb0c91d7d6c1c21e937ff1cb9fd36c461c7
NdivhoNemz <ndivhonematandani@gmail.com>
2023-09-27 18:47:15 -0700

    update

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,7 +10,7 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
-		return (-1);
+		return (-1); /* error returned */
 	}
 	return (new_sqrt_recursion(n, 0));
 }
@@ -30,7 +30,7 @@ int new_sqrt_recursion(int n, int i)
 	}
 	if (i * i == n)
 	{
-		return (i);
+		return (i); /* natural number */
 	}
 	return (new_sqrt_recursion(n, i + 1));
 }
