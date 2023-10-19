cae6bd11726341395fd682e49660929e84259f35
NdivhoNemz <ndivhonematandani@gmail.com>
2023-10-19 08:51:46 -0700

    100-first.c:8: error: cannot understand function prototype: ' void print_before()' QUICK FIX

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
  * Return: void
  */
 
-__attribute__((constructor)) void print_before(void)
+void print_before(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
